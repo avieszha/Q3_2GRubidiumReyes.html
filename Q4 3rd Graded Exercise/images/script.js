ff9990cefe7928f95707a6c86f74c3f3916edfa9
avieszha <b2029amfpreyes@pshs.edu.ph>
2026-03-31 22:06:16 +0800

    ss

--- Q4 3rd Graded Exercise/images/script.js
+++ Q4 3rd Graded Exercise/images/script.js
@@ -1,14 +1,17 @@
-function plotPoint() {
+function plotPoint(x0, y0, x, y) { //uwu fixed
     console.log(x0, y0, x, y) // checks the arguments passed to this function
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
     var point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property | fixed!
+    point.style.bottom = (y - y0 + 200 - 5) + 'px';// subtract half the height of the point to center it by changing bottom css property | fixed!
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x > x0 && y > y0){
+    if (x === x0 || y === y0){
+      out1.innerHTML = "divisa"
+    }
+    else if (x > x0 && y > y0){
       out1.innerHTML = "NE"
     }
     else if (x < x0 && y < y0){
@@ -20,6 +23,4 @@ function plotPoint() {
     else if (x < x0 && y > y0){
       out1.innerHTML = "NO"
     }
-    else{
-      out1.innerHTML = "divisa"
-    }
+  } // fixed; no "}""
